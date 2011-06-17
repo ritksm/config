c54396d5b23ec5b1c4addb36997345218bc9e213
Jack River <ritksm@gmail.com>
2011-06-18 02:55:11 +0800

    add config files and an autobackup bash script

---
+++ autobackup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cp ~/.config/awesome ./ -R
+cp ~/.xinitrc ./x11
+cp ~/.xprofile ./x11
+cp ~/.bashrc ./
+cp ~/.vimrc ./vim/
+cp ~/.vim ./vim/ -R
+cp /etc/make.conf ./gentoo_config/
+cp /etc/fstab ./gentoo_config/
+cp /etc/portage ./gentoo_config/ -R
+cp /etc/env.d/99* ./gentoo_config/env.d/
+cp /etc/locale.gen ./gentoo_config/
+cp /etc/rc.conf ./gentoo_config/
+
