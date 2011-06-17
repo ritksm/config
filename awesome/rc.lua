a276269932d48df391b11787baa02a737ea7ae2d
Jack River <ritksm@gmail.com>
2011-06-18 05:25:02 +0800

    add lock screen fuction to awesome usign xscreensaver

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -217,6 +217,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey,           }, "F12",   function () awful.util.spawn("xscreensaver-command -lock") end),  
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
