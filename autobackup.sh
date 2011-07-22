016ee24386d118c4efc9978a9e971e71dcb16348
Jack River <ritksm@gmail.com>
2011-07-22 15:08:00 +0800

    add git config

--- autobackup.sh
+++ autobackup.sh
@@ -12,4 +12,4 @@ cp /etc/env.d/99* ./gentoo_config/env.d/
 cp /etc/locale.gen ./gentoo_config/
 cp /etc/rc.conf ./gentoo_config/
 cp /usr/src/linux/.config ./gentoo_config/kernel_config
-
+cp ~/.gitconfig	./gitconfig
